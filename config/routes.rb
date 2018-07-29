ba70992a41b10b730538bf38660607cc203ce9ad
Emma Laroche <e.r.laroche@gmail.com>
2018-07-28 23:35:27 -0500

    initial commit, btw, check application.js for require jquery and require jquery_ujs when using foundation zurb top-bar..

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'welcome/index', :as => 'home'
+  get 'welcome/services', :as => 'services'
+  get 'welcome/testimonials', :as => 'testimonials'
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'welcome#index'
+end
